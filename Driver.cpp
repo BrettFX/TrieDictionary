96f1d6e67130234f022b08bc39d8a0e44d4ebfdc
Brett Allen <brettallen777@gmail.com>
2017-01-26 15:37:58 -0500

    Added a display method for testing purposes
    Display method takes an input string, parses through it and displayes
    each individual character to the console.

--- Driver.cpp
+++ Driver.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
+#include <cstdlib>
+#include <cstdio>
 
 using namespace std;
 
+void display(char word[]);
+
 int main(int argc, char** argv)
 {
-	cout << "Hello World!\n";
+	char input[256];
+	
+	cout << "Please enter a word to be parsed: ";
+	scanf("%100[^\n]", input);
+	
+	display(input);
 	
 	return 0;
+}
+
+void display(char* word)
+{
+	if(*word != '\0')
+	{
+		cout << "Your word: ";
+		
+		while(*word != '\0')		
+			cout << *word++ << "\t";
+		
+		cout << "\n";
+	}
+	else
+		cout << "No word to display\n\n";
 }
